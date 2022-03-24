400a5e1abd256bb185608f7edf60c65f27e6f2d5
gurvi20 <gurvindersingh201996@gmail.com>
2022-03-23 21:47:27 -0400

    one user can't edit and delete  others places

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -77,9 +77,9 @@ const PlaceItem = (props) => {
             <Button inverse onClick={OpenMapHandler}>
               VIEW ON MAP
             </Button>
-            {auth.isLoggedIn&&
-            <Button to={`/places/${props.id}`}>EDIT</Button>}
-             {auth.isLoggedIn &&(
+            {auth.userId ===props.creatorId &&(
+            <Button to={`/places/${props.id}`}>EDIT</Button>)}
+             {auth.userId ===props.creatorId &&(
             <Button danger onClick={OpenConfirmDeleteModal}>DELETE</Button>)}
           </div>
         </Card>
